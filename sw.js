8f875951d1c1a84e9d42c9a4c2624f666c72fe36
Hasan Mahmud <163021899+Hasanmahmud000@users.noreply.github.com>
2025-06-19 17:10:01 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cricZone-v7"; // প্রতিবার update দিলে version change করো
+const CACHE_NAME = "cricZone-v8"; // প্রতিবার update দিলে version change করো
 
 const urlsToCache = [
   "/",
